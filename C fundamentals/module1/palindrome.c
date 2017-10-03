b4aaa1c7961e72d626b5e1db87b3e878d6bf857c
jareyeshurtado <jareyeshurtado@gmail.com>
2017-10-03 09:55:14 +0200

    added some string exercises

---
+++ C fundamentals/module1/palindrome.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#define START 0
+
+
+
+int main(int argc, char const *argv[]) {
+  const int to_lower_case = 'a' - 'A';
+  char a[] = "SOc";
+  int size = strlen(a);
+  char b[size];
+int result;	
+
+  for(int i = START; i < size; i++)
+  {
+    if(a[(size-1)-i] >= 'A' && a[(size-1)-i] <= 'Z')
+      a[(size-1)-i] += to_lower_case;
+    b[i] = a[(size-1)-i];
+    
+  }
+  b[size] = '\0';
+  if(strcmp(a,b))
+    printf("The string is not palindrome :(\n");
+  else
+    printf("The string is a palindrome!!!!\n");
+  return 0;
+}
